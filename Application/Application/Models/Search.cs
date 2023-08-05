35ac15985ef119a9cbd53a81aae5b9451555e08d
İrem Özben <56583531+iremozbn@users.noreply.github.com>
2023-08-05 23:13:18 +0300

    Update Search.cs

--- Application/Application/Models/Search.cs
+++ Application/Application/Models/Search.cs
@@ -1,18 +1,14 @@
-﻿namespace Application.Models
+namespace Application.Models
 {
     public class Search
     {
-        public class SearchRequest
-        {
-            public int Id { get; set; }
+        public class SearcMeetingRequest
+        { 
             public string TeamName { get; set; }
-            public string MeetingName { get; set; }
         }
         public class SearchEventRequest
         {
-            public int Id { get; set; }
             public string EventName { get; set; }
-            public string EventType { get; set; }
         }
     }
 }
